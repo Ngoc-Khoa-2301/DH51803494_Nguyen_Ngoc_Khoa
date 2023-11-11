12c67f6c368afa55632df3b9c2aefd7e91e13903
Ngoc-khoa-2301 <kenkonoob23@gmail.com>
2023-11-11 16:39:51 +0700

    nhap so

---
+++ c++.cpp
@@ -0,0 +1,10 @@
+#include <iosteam>
+using namespace std;
+int main()
+{
+    int n;
+    cout << "Nhap so nguyen n: ";
+    cin >> n;
+    cout << "So nguyen vua nhap: " << n ;
+    return n;   
+}
